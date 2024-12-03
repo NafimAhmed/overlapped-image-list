4cef57f04a102b2575846dc9e37e13a48b4729e0
recent <recentnafimahmed@gmail.com>
2024-12-03 22:46:18 +0600

    init commit

---
+++ lib/overlapped_image_list.dart
@@ -0,0 +1,49 @@
+library overlapped_image_list;
+
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+/// A Calculator.
+class OverLappedImageList extends StatelessWidget {
+  final List<String> imageList;
+  final double borderWidth;
+  final double imageRadious;
+  final Color borderColor;
+
+  const OverLappedImageList(
+      {super.key,
+      required this.imageList,
+      this.borderWidth = 0,
+      this.imageRadious = 20,
+      this.borderColor = Colors.white});
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return SingleChildScrollView(
+        scrollDirection: Axis.horizontal,
+        child: Container(
+          // parent row
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              for (int i = 0; i < imageList.length; i++)
+                Align(
+                  widthFactor: 0.5,
+                  // parent circle avatar.
+                  // We defined this for better UI
+                  child: CircleAvatar(
+                    radius: imageRadious + borderWidth,
+                    backgroundColor: borderColor,
+                    // Child circle avatar
+                    child: CircleAvatar(
+                      radius: imageRadious,
+                      backgroundImage: NetworkImage(imageList[i]),
+                    ),
+                  ),
+                )
+            ],
+          ),
+        ));
+  }
+}
